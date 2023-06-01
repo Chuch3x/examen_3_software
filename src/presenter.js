8d3d520b1d1fd5fd70c54fecab9d9b911e6cd89f
Cesar <c.augusto_mendez@hotmail.com>
2023-06-01 09:24:10 -0400

    si se ingresan campos vacio se lanza una alerta para llenar los campos

--- src/presenter.js
+++ src/presenter.js
@@ -7,9 +7,17 @@ const div = document.querySelector("#resultado_div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  if (monto.value === "" || efectivo.value === "") {
+    alert("Error, llene los campos por favor");
+  } else {
+    const monto_venta = Number.parseInt(monto.value);
+    const efectivo_venta = Number.parseInt(efectivo.value);
 
-  const monto_venta = Number.parseInt(monto.value);
-  const efectivo_venta = Number.parseInt(efectivo.value);
-
-  div.innerHTML = "<p>" + "Cambio: " + calcular_cambio(monto_venta, efectivo_venta) + " Bs."+"</p>";
+    div.innerHTML =
+      "<p>" +
+      "Cambio: " +
+      calcular_cambio(monto_venta, efectivo_venta) +
+      " Bs." +
+      "</p>";
+  }
 });
