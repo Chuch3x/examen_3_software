6cc306cbb54009250477c6d9efd09d15b02c07cb
Cesar <c.augusto_mendez@hotmail.com>
2023-06-01 09:28:13 -0400

    Se crea la  prueba para calcular la cantidad de monedas

--- src/calcular_monto.test.js
+++ src/calcular_monto.test.js
@@ -4,4 +4,7 @@ describe("Calcular", () => {
   it("deberia mostrar el cambio de la venta", () => {
     expect(calcular_cambio(20, 30)).toEqual(10);
   });
+  it("deberia mostrar la cantidad de monedas a entregar de cambio", () => {
+    expect(calculacalcular_monedas(13)).toEqual([5,5,2,1]);
+  });
 });
